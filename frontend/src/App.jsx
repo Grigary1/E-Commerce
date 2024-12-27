ca8bc29f3cb942bea7a1959d13367df5098d31e5
Grigary1 <grigaryvarkey16@gmail.com>
2024-12-27 11:22:25 +0530

    added rooutes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Collections from './pages/Collections'
+import About from './pages/About'
+import Product from './pages/Product'
+import Contact from './pages/Contact'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
 
 const App = () => {
   return (
-    <div className='bg-red-700'>
-      hello
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9nw]'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collections/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+      </Routes>
     </div>
   )
 }
