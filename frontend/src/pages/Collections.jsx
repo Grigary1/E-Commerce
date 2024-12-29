4e17fa34e7e127e5c40bcd6ff007f7bffabaddf8
Grigary1 <grigaryvarkey16@gmail.com>
2024-12-29 11:54:47 +0530

    added dropdown

--- frontend/src/pages/Collections.jsx
+++ frontend/src/pages/Collections.jsx
@@ -1,43 +1,64 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Title from '../components/Title'
+import { shopContext } from '../context/ShopContext'
+import { images } from '../assets/assets'
 
 const Collections = () => {
+
+  const { products } = useContext(shopContext);
+  const [showFilter, setShowFilter] = useState(false);
+
   return (
-    <div className='flex flex-row'>
-      {/* Filters */}
-      <div>
-        <p>FILTERS</p>
-        {/* Categories */}
-        <div className='border border-gray-600 w-40 p-4 mb-2'>
-          Categories
-          <form>
-            <input type="checkbox" name="" id="" />
-            <label>  Men</label><br/>
-            <input type="checkbox" name="" id="" />
-            <label>  Women</label><br/>
-            <input type="checkbox" name="" id="" />
-            <label>  Kids</label><br/>
-          </form>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+
+      {/* Filter options */}
+      <div className='min-w-60'>
+        <div onClick={() => setShowFilter((prev) => !prev)} className='flex items-center cursor-pointer gap-2'>
+          <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+          <img className={`h-3 sm:hidden ${showFilter ? '' : 'rotate-180'}`} src={images.dropdown_icon} alt="" />
+        </div>
+        {/* Category */}
+        <div
+  className={`sm:border sm:border-gray-300 pl-5 py-3 mt-6 transition-all duration-300 ease-in-out overflow-hidden ${
+    showFilter
+      ? 'max-h-[500px]'
+      : 'max-h-0 border-none py-0 pl-0 mt-0'
+  } sm:max-h-none sm:block`}
+>
+          <p className="mb-3 text-sm font-medium">CATEGORIES</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" name="" id="" value={'Men'} />Men
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" name="" id="" value={'Women'} />Women
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" name="" id="" value={'Kids'} />Kids
+            </p>
+          </div>
         </div>
 
-        {/* Type */}
-        <div className='border border-gray-600 w-40 p-4'>
-          Categories
-          <form>
-            <input type="checkbox" name="" id="" />
-            <label>  TopWear</label><br/>
-            <input type="checkbox" name="" id="" />
-            <label>  Bottomwear</label><br/>
-            <input type="checkbox" name="" id="" />
-            <label>  Winterwear</label><br/>
-          </form>
+
+        {/* Sub category */}
+        <div className={`border border-gray-300 pl-5 py-3 my-5 transition-all duration-300 ease-in-out overflow-hidden ${showFilter ? '' : 'hidden'} sm:block`}>
+          <p className='mb-3 text-sm font-medium'>TYPE</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" name="" id="" value={'Topwear'} />Topwear
+            </p>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" name="" id="" value={'Bottomwear'} />Bottomwear
+            </p>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" name="" id="" value={'Winterwear'} />Winterwear
+            </p>
+          </div>
         </div>
+
+
       </div>
 
-      {/* All collections */}
-      <div>
-        <Title></Title>
-      </div>
     </div>
   )
 }
