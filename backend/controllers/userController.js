1b15e8deb69783319cf8b813d0e18579938682dd
Grigary1 <grigaryvarkey16@gmail.com>
2025-01-09 18:15:32 +0530

    added auth middle ware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -59,6 +59,17 @@ export default registerUser;
 
 //Route for admin login
 const adminLogin = async (req, res) => {
-
+    try {
+        const {email,password}=req.body;
+        if (email===process.env.ADMIN_EMAIL && password===process.env.ADMIN_PASSWORD){
+            const token=jwt.sign(email+password,process.env.JWT_SECRET);
+            return res.status(200).json({success:true,token});
+        }
+        else{
+            return res.json({success:false,message:"Invalid credentials"});
+        }
+    } catch (error) {
+        return res.send("False");
+    }
 }
 export { loginUser, registerUser, adminLogin };
