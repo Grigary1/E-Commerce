92001f4f0cfe89ea92baec55b9951a71ef6ec38d
Grigary1 <grigaryvarkey16@gmail.com>
2025-01-10 17:06:46 +0530

    created admin panel

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,9 @@
+import logo from './../assets/logo.png'
+import add_icon from './../assets/add_icon.svg'
+import list_icon from './../assets/list_icon.png'
+import orders_icon from './../assets/orders_icon.png'
+export const assets={
+    logo,
+    add_icon,
+    list_icon,orders_icon
+}
